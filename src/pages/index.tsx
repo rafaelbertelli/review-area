2e3c1fc576b6691cf34bc8757543a7cc820b624c
Rafael Borges <rafael.borges@luizalabs.com>
2022-06-16 09:52:55 -0300

    reorg initial structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
+        <h1 className="text-3xl font-bold underline font-yellow">
+          Hello world!
+        </h1>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
