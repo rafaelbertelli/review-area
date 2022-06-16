4b47c5b04cabde1c3dd28951015ec43a3984d9ee
Rafael Borges <rafaelbertelli89@gmail.com>
2022-06-16 13:53:28 -0300

    use login as page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,9 @@
+import Login from "../../components/Login";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <Login />
+    </div>
+  );
+}
