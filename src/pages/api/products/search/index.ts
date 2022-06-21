31f9e0d569dc4453747c4a8497c0cbc26d889688
Rafael Borges <rafaelbertelli89@gmail.com>
2022-06-20 22:02:57 -0300

    return products

--- src/pages/api/products/search/index.ts
+++ src/pages/api/products/search/index.ts
@@ -10,11 +10,7 @@ export default async function handler(
   return axios
     .get(`https://api.mercadolibre.com/sites/MLB/search?q=${term}`)
     .then((response) => {
-      const items = response.data;
-      console.log(items);
-      console.log("search");
-      console.log(">>>", term);
-
-      res.status(200).json({ items });
+      const products = response.data;
+      res.status(200).json({ products });
     });
 }
